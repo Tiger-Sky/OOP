87bb7e9d8aa397175c1f1ba30647147620e13b28
swcyen <130520821+swcyen@users.noreply.github.com>
2023-04-26 13:52:11 +0700

    check again

--- bai8/run.java
+++ bai8/run.java
@@ -11,7 +11,7 @@ public class run {
                 System.out.println("chon 1-4");
                 System.out.println("chon 1: them the");
                 System.out.println("chon 2: Xoa the");
-                System.out.println("chon 3: Hien thi");
+                System.out.println("chon 3: Hien thi");//check again
                 System.out.println("chon 4: de thoat"); // check
                 int i = scanner.nextInt();
                 switch(i)
